4c2792f26995b382d1fb9810351978400ceab593
Lopez John Jairo <72105633+lopezjohnjairo5@users.noreply.github.com>
2023-10-15 15:07:37 -0500

    update
    corrección del hover nav

--- public/js/main.js
+++ public/js/main.js
@@ -3,19 +3,21 @@ control de navbar
 */
 
 let navItems = document.getElementById("navbar-items"),
+    elements = document.getElementsByClassName("goto"),
     btnHomeProject = document.getElementById("btn-home-goto-project");
 
 const hiddeShowElements = (seeElement) => {
-    let elements = document.getElementsByClassName("goto"),
         element = seeElement;
         
     for (let i = 0; i < elements.length; i++) {
-        //console.log(`i: ${i}`);
         el = elements[i].dataset.go;
         if (el == element) {
             document.getElementById(el).style.display = "flex";
+            elements[i].parentNode.style.backgroundColor = "rgba(69,69,69,1)";
         } else {
             document.getElementById(el).style.display = "none";
+            elements[i].parentNode.style.backgroundColor = "#FFB633";
+            elements[i].style.color = "white";
         }
         
     } 
@@ -27,7 +29,6 @@ navItems.addEventListener("click", (e) =>{
         idEl = element.dataset.go;
         hiddeShowElements(idEl); 
     }
-
 });
 
 btnHomeProject.addEventListener("click", () => {
